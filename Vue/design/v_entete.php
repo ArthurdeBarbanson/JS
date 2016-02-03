70f50498dc4e4c792c8fb5d373ffee04a682e378
ArthurdeBarbanson <arthurdebarbanson@gmail.com>
2016-02-04 00:31:08 +0100

    maj favicon

--- Vue/design/v_entete.php
+++ Vue/design/v_entete.php
@@ -8,8 +8,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
+    <link rel="icon" type="image/png" href="../../Css/img/Uiconstock-Socialmedia-Picasa.ico" />
 
-    <title>Picasa</title>
+
+    <title>Picasa 2.0</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="Css/css/bootstrap.min.css" rel="stylesheet">
