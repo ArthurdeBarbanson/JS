41bb37e78f0e787f77fc1a6307ec5fe979f8dbb8
modoux <remi.modoux@gmail.com>
2016-02-08 12:04:39 +0100

    ajout bibli modif carousel

--- Vue/design/v_piedpage.php
+++ Vue/design/v_piedpage.php
@@ -13,14 +13,11 @@
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
 
-    <!-- Morris Charts JavaScript -->
-            <script src="js/plugins/morris/raphael.min.js"></script>
-            <script src="js/plugins/morris/morris.min.js"></script>
-            <script src="js/plugins/morris/morris-data.js"></script>
     <!-- Carousel -->
+
+            <script type="text/javascript" src="js/fancybox/source/jquery.fancybox.js?v=2.1.5"></script>
             <script type="text/javascript" src="js/jssor.slider.mini.js"></script>
             <script type="text/javascript" src="js/carousel.js"></script>
-            <script type="text/javascript" src="js/fancybox/source/jquery.fancybox.js"
 
 </body>
 
