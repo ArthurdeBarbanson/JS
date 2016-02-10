8eab16233e266d6f5b56a5e570185606a0e31c11
ArthurdeBarbanson <arthurdebarbanson@gmail.com>
2016-02-10 19:27:37 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Vue/design/v_piedpage.php

---
+++ Modele/upload.php
@@ -0,0 +1,43 @@
+<?php //C'est $_FILES qui "transporte" nos fichiers
+$chemin='/home/arthur/Documents/Cours/JavaScript/imagestest/';
+if(!empty($_FILES)){
+    //on compte le nombre de fichiers re�u par le serveur
+    $nb=count($_FILES['file']['name']);
+    if($nb>0){
+        //on boucle sur le nombre de fichier
+        for($i=0;$i<$nb;$i++){
+            //$_FILES['file']['error'][$i] retourne 0 si pas d'erreurs et retourne 1,2,3 ou 4 en cas d'erreur
+            if ($_FILES['file']['error'][$i]>0) {
+                switch ($_FILES['file']['error'][$i]){
+                    case 1:
+                        echo $_FILES['file']['name'][$i]."<h2> Erreur : le fichier d�passe la limite autoris�e par le serveur !</h2>";
+                        break;
+                    case 2:
+                        echo $_FILES['file']['name'][$i]." <h2>Erreur : le fichier d�passe la limite autoris�e !</h2>";
+                        break;
+                    case 3:
+                        echo $_FILES['file']['name'][$i]." <h2>Erreur : l'envoi du fichier a �t� interrompu pendant le transfert !</h2>";
+                        break;
+                    case 4:
+                        echo "pas d'erreurs";
+                        break;
+                }
+            }
+            else
+            {
+                $image=$_FILES['file']['name'][$i];
+                //on d�place les fichiers du repertoire temporaire vers un repertoire "sauv_up"
+                $copie=move_uploaded_file($_FILES['file']['tmp_name'][$i],$chemin.$image);
+
+                //enregistrement bd
+
+            }
+        }
+    }
+}
+
+var_dump($_FILES);var_dump($copie);
+//var_dump($image);
+//var_dump($extension);
+//var_dump($_FILES['file']['tmp_name'][0]);
+//var_dump('../Modele/testupload/'.$image);
--- Vue/v_Album.php
+++ Vue/v_Album.php
@@ -1,23 +1,96 @@
-<!-- Page Heading -->
-<div class="row">
-    <div class="col-lg-12">
+<form action="/JS/Modele/upload.php" method="post" enctype="multipart/form-data">
+    <input type="hidden" name ="MAX FILE SIZE" value="2000000">
+    <input id="file" type="file"  name="file[]"  accept="image/*" multiple>
+    <button type="submit" class="btn btn-primary btn-lg" name="btn_newDemande">Envoyer</button>
+
+</form>
+
+<!--<input type="button" value="uploader" onclick="uploader()">-->
+
+<div id="prev"></div>
+<progress id="progress"></progress>
+
+<script>
+    (function() {
+
+        function createThumbnail(file) {
+
+            var reader = new FileReader();
+
+            reader.addEventListener('load', function() {
+
+                var imgElement = document.createElement('img');
+                imgElement.style.maxWidth = '150px';
+                imgElement.style.maxHeight = '150px';
+                imgElement.style.margin = '5px';
+                imgElement.src = this.result;
+                prev.appendChild(imgElement);
+
+            }, false);
+
+            console.log("reader : "+reader);
+//            console.log(reader.readAsDataURL(file));
+            reader.readAsDataURL(file);
+        }
+
+        var allowedTypes = ['png', 'jpg', 'jpeg', 'gif'],
+            fileInput = document.querySelector('#file'),
+            prev = document.querySelector('#prev');
+
+        fileInput.addEventListener('change', function() {
+
+            var files = this.files,
+                filesLen = files.length,
+                imgType;
+            var formData = new FormData();
+
+            for (var i = 0 ; i < filesLen ; i++) {
+
+                imgType = files[i].name.split('.');
+                imgType = imgType[imgType.length - 1];
+
+                if(allowedTypes.indexOf(imgType) != -1) {
+                    createThumbnail(files[i]);
+                    formData.append('file', fileInput.files[i]);
+                }
+
+            }
+
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', '/JS/Modele/upload.php'); // Rappelons qu'il est obligatoire d'utiliser la méthode POST quand on souhaite utiliser un FormData
+
+            xhr.upload.addEventListener('progress', function(e) {
+                progress.value = e.loaded;
+                progress.max = e.total;
+            }, false);
+
+            xhr.addEventListener('load', function() {
+                alert('Upload terminé !');
+            }, false);
+
+            xhr.send(formData);
+
+        }, false);
 
 
-        <!-- The fileinput-button span is used to style the file input field as button -->
-    <span class="btn btn-success fileinput-button">
-        <i class="glyphicon glyphicon-plus"></i>
-        <span>Add files...</span>
-        <!-- The file input field used as target for the file upload widget -->
-        <input id="fileupload" type="file" name="files[]" multiple>
-    </span>
-        <br>
-        <br>
-        <!-- The global progress bar -->
-        <div id="progress" class="progress">
-            <div class="progress-bar progress-bar-success"></div>
-        </div>
-        <!-- The container for the uploaded files -->
-        <div id="files" class="files"></div>
-    </div>
-</div>
 
+    })();
+
+    function upload(){
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/JS/Modele/upload.php'); // Rappelons qu'il est obligatoire d'utiliser la méthode POST quand on souhaite utiliser un FormData
+
+        xhr.upload.addEventListener('progress', function(e) {
+            progress.value = e.loaded;
+            progress.max = e.total;
+        }, false);
+
+        xhr.addEventListener('load', function() {
+            alert('Upload terminé !');
+        }, false);
+
+        xhr.send(formData);
+    }
+
+</script>
